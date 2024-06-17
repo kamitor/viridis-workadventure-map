0140e684ecc042f67f712180166b02985ed1fa10
Nicolas Wild <nw@viridis.info>
2024-06-17 20:13:34 +0200

    Add assets

---
+++ tilesets/VIRIDIS-logo-32.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="git" name="VIRIDIS-logo-32" tilewidth="32" tileheight="32" tilecount="5" columns="5">
+ <image source="VIRIDIS-logo-32.png" width="160" height="32"/>
+</tileset>
---
+++ tilesets/VIRIDIS-logo.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="git" name="VIRIDIS-logo" tilewidth="32" tileheight="32" tilecount="20" columns="10">
+ <image source="VIRIDIS-logo.png" width="320" height="80"/>
+</tileset>
